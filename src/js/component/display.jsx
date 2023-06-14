88af00fc83457f41d4637bed40a458d7a47258fe
Rui Silva <rsilva6420@gmail.com>
2023-06-14 14:34:26 +0000

    Version2 - final with animated background

--- src/js/component/display.jsx
+++ src/js/component/display.jsx
@@ -5,9 +5,7 @@ const Display = () => {
 	const [city, setCity] = useState('');
     const [weatherDetail, setWeatherDetail] = useState({});
     const [showWeather, setShowWeather] = useState(false); // this is a flag
-
-
-
+	
     const handleChange = (e) => {
         setCity(e.target.value);
       };
@@ -36,32 +34,78 @@ const Display = () => {
 
 	return (
 		<div>
-			<div className="row justify-content-center pt-2 pb-2">
-				<div className="col-6">
-					<input 
-						class="form-control" 
-						type="text" 
-						placeholder="search for a city" 
-						value={city}
-						onChange={handleChange}
-						onKeyDown={handleSubmit}
-					></input>
+			<div class="bg"></div>
+			<div class="bg bg2"></div>
+			<div class="bg bg3"></div>
+			<div class="content">
+				<div className="row justify-content-center pt-2 pb-2 mb-2">
+					<div className="col-6">
+						<input 
+							class="form-control" 
+							type="text" 
+							placeholder="search for a city" 
+							value={city}
+							onChange={handleChange}
+							onKeyDown={handleSubmit}
+						></input>
+					</div>
 				</div>
-				
-			</div>
-			<div>
-                {
-                    showWeather && (
-                        <div>
-                            <p>{weatherDetail.temperature}</p>
-                            <p>{weatherDetail.wind}</p> 
-                            <p>{weatherDetail.description}</p>
-                            <h3>Forecast for next 3 days</h3>
-                            <p>{weatherDetail.forecast[0].temperature}</p>
-                            <p>{weatherDetail.forecast[0].wind}</p>                           
-                        </div>
-                    )
-                }
+				<div>
+					{showWeather && (
+							<div>
+								<div className="container text-center mt-5">
+									<div className="row justify-content-md-center">
+										<div className="col-4">
+											<h6>CURRENT WEATHER</h6>
+											<div>
+												<span>
+													{(() => {
+														let tag;
+														if (weatherDetail.description === "Partly cloudy") tag = <div><i class="fa-solid fa-cloud-sun icon"></i><p>Partly cloudy</p></div>;
+														else if (weatherDetail.description === "Light rain") tag = <div><i class="fas fa-cloud-rain icon"></i><p>Light rain</p></div>;
+														else if (weatherDetail.description === "Light rain shower") tag = <div><i class="fas fa-cloud-sun-rain icon"></i><p>Light rain shower</p></div>;
+														else if (weatherDetail.description === "Patchy rain possible") tag = <div><i class="fas fa-poo-storm icon"></i><p>Patchy rain possible</p></div>;
+														else if (weatherDetail.description === "Rain with thunderstorm, heavy rain with thunderstorm") tag = <div><i class="fas fa-umbrella icon"></i><p>Rain with thunderstorm, heavy rain with thunderstorm</p></div>;
+														else if (weatherDetail.description === "Heavy rain with thunderstorm") tag = <div><i class="fas fa-umbrella icon"></i><p>Heavy rain with thunderstorm</p></div>;
+														else if (weatherDetail.description === "Rain with thunderstorm") tag = <div><i class="fa-solid fa-poo-storm icon"></i><p>Rain with thunderstorm</p></div>;
+														else if (weatherDetail.description === "Shower in vacinity, rain shower") tag = <div><i class="fa-solid fa-cloud-showers-water icon"></i><p>Shower in vacinity, rain shower</p></div>;
+														else if (weatherDetail.description === "Sunny") tag = <div><i class="fas fa-sun icon"></i><p>Sunny</p></div>;
+														else if (weatherDetail.description === "Clear") tag = <div><i class="fas fa-sun icon"></i><p>Clear</p></div>;
+														else tag = weatherDetail.description
+														return tag;
+													})()}
+												</span>
+											</div>
+										</div>
+										<div className="col-3">
+											<h6>TEMPERATURE</h6>
+											<div>
+												<span>
+													<div>
+														<i class="fa-solid fa-temperature-half icon"></i>
+														{<p>{weatherDetail.temperature}</p>}
+													</div>
+													
+												</span>
+											</div>
+										</div>
+										<div className="col-2">
+											<h6>WIND</h6>
+											<div>
+												<span>
+													<div>
+														<i class="fas fa-wind icon"></i>
+														{<p>{weatherDetail.wind}</p>}
+													</div>
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>                   
+							</div>
+						)
+					}
+				</div>
 			</div>
 		</div>
 	);
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import Display from "./display.jsx";
 
+import image from "/src/img/wolfgang-hasselmann-bR_-gllg7Bs-unsplash_1.png"
+
 const Home = () => {
 	
 
 	return (
 		<div className="text-center">
-			<h1 className="pt-5 pb-5 text-success">Weather In Your City</h1>
+			<div className="header" style={{ backgroundImage:`url(${image})`}}>
+				<h1 className="pt-5 pb-5 text-success text-white title"><strong>Weather In Your City</strong></h1>
+			</div>
 			<Display />	
 		</div>
 	);
